3ff44c44223b0c87dd02698e28770f86a8cfb6d4
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-05-01 00:30:23 +0300

    bugfixing

--- first-nodejs-app/calculateCircle.js
+++ first-nodejs-app/calculateCircle.js
@@ -2,4 +2,4 @@ const parametre = process.argv.slice(2);
 
 const calculateArea = (halfDiameter) => Math.PI * (parametre**2)
 
-console.log(calculateArea(parametre))
+console.log(calculateArea(Number(parametre[0])))
